bacf43c026e85ba6fb6ed4ac8e6763c69fd0da02
aparnadevigm <aparnach1269@gmail.com>
2024-12-04 14:50:57 +0530

    show connection requests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -37,12 +37,16 @@ const Requests = () => {
         const { _id,firstName, lastName, age, gender, skills, about, photoUrl } =
           request.fromUserId;
         return (
-          <div key={_id}className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto">
-            <img
+          <div key={_id}className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-2/3 mx-auto">
+            
+            <div>
+              <img
               className="w-20 h-20 rounded-full"
               src={photoUrl}
               alt="photo"
             />
+              </div>
+            
 
             <div className="text-left mx-4">
               <h2 className="font-bold text-xl">
@@ -51,6 +55,10 @@ const Requests = () => {
               {age && gender && <p>{age + " " + gender}</p>}
               <p>{about}</p>
             </div>
+            <div>
+              <button className="btn btn-primary mx-2">Reject</button>
+              <button className="btn btn-secondary mx-2">Accept</button>
+              </div>
           </div>
         );
       })}
